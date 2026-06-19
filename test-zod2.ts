2e577117e6de473c89f52887839274ddf370ed7b
MUHAMMAD SHAHALSHAD <shahallshad@gmail.com>
2026-06-19 17:20:50 +0530

    Remove debug test files

--- test-zod2.ts
+++
@@ -1,8 +0,0 @@
-import { JobVacancySchema } from './src/lib/schemas';
-
-const result = JobVacancySchema.safeParse({});
-if (!result.success) {
-  console.log("has errors:", !!result.error.errors);
-  console.log("has issues:", !!result.error.issues);
-  console.log("keys:", Object.keys(result.error));
-}
